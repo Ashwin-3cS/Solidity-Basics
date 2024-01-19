f080aa185e1a14b6eb97d9e4fa49a460d5551cb0
Ashwin-3cS <flashashwin2004@gmail.com>
2024-01-19 15:08:10 +0530

    constants in solidity

---
+++ constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    address public constant  MY_ADDRESS = 0xbb7462adA69561Ff596322A2f9595c28E47FD6aa;
+    uint public constant MY_UINT = 123;
+}
+// while using constants the gas cost will be reduced check it
